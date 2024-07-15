03a025db6cf4cf51fa92e0ab75f0efeaaf085edf
untoldextacy <vanlarec1991@gmail.com>
2024-07-15 19:45:40 -0400

    styling, mostly has conflicts

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// Strength of the Infinity Stones
+
+const rootMan = $('#root');
+const infinity = $('<p>');
+
+infinity.text = ('~Thanos')
+
+infinity.addClass('.footer')
+const motto = $('<h4>')
+
+motto.text('Strength of the Infinity Stones')
+motto.addClass('power');
+
+console.log(infinity);
+
+motto.append(infinity);
+
+rootMan.append(motto);
